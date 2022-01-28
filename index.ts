1375a66ec6798ee3549d590f6f2b83c8bd943d67
Patrick Quinn-Graham <make-contact@pftqg.com>
2022-01-29 10:18:52 +1100

    Add github oidc

---
+++ index.ts
@@ -0,0 +1,47 @@
+import * as aws from '@pulumi/aws';
+
+export const oidcProvider = new aws.iam.OpenIdConnectProvider('github-oidc', {
+  url: 'https://token.actions.githubusercontent.com',
+  clientIdLists: ['sts.amazonaws.com'],
+  thumbprintLists: ['6938fd4d98bab03faadb97b34396831e3780aea1'],
+});
+
+export const pulumiMergeRole = new aws.iam.Role('thepatrick/thepatrick.cloud.tf/merge', {
+  namePrefix: 'thepatrick-cloud-tf-actions-merge',
+  managedPolicyArns: ['arn:aws:iam::aws:policy/AdministratorAccess'],
+  assumeRolePolicy: {
+    Version: '2012-10-17',
+    Statement: [
+      {
+        Effect: 'Allow',
+        Action: 'sts:AssumeRoleWithWebIdentity',
+        Principal: { Federated: oidcProvider.arn },
+        Condition: {
+          StringLike: {
+            'token.actions.githubusercontent.com:sub': ['repo:thepatrick/thepatrick.cloud.tf:ref:refs/heads/main'],
+          },
+        },
+      },
+    ],
+  },
+});
+
+export const pulumiPreviewRole = new aws.iam.Role('thepatrick/thepatrick.cloud.tf/preview', {
+  namePrefix: 'thepatrick-cloud-tf-actions-pr',
+  managedPolicyArns: ['arn:aws:iam::aws:policy/AdministratorAccess'],
+  assumeRolePolicy: {
+    Version: '2012-10-17',
+    Statement: [
+      {
+        Effect: 'Allow',
+        Action: 'sts:AssumeRoleWithWebIdentity',
+        Principal: { Federated: oidcProvider.arn },
+        Condition: {
+          StringLike: {
+            'token.actions.githubusercontent.com:sub': ['repo:thepatrick/thepatrick.cloud.tf:ref:refs/heads/main'],
+          },
+        },
+      },
+    ],
+  },
+});
